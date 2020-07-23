66fea51c432c2d26686c395f998ca3eea681b355
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-07-23 02:08:15 -0400

    Finished makeDino

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-const makeDino = function() {
+const makeDino = function(species, period, diet, extinct) {
+  const dino = {};
+    dino.species = species;
+    dino.period = period;
+    dino.carnivore = diet;
+    dino.extinct = false;
+    
+    if(extinct !== undefined) {
+      dino.extinct = extinct;
+    }
+
+  return dino;
 }
 
 const makeSingular = function() {
